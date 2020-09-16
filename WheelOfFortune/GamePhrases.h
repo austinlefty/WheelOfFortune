ab764cfc16d400ed1b31af6f52a5ddb5d1f98c02
austinlefty <leftykrause@hotmail.com>
2020-09-16 12:47:14 -0500

    Comments

--- WheelOfFortune/GamePhrases.h
+++ WheelOfFortune/GamePhrases.h
@@ -3,38 +3,37 @@
 #include <fstream>
 #include <string>
 #include <vector>
+
 using namespace std;
 class GamePhrases
 {
 private:
-    vector <string> list;
-    string word;
-    ifstream infile;
+    vector <string> list; //Containter for words
+    string word; //Copys word from txt file
+    ifstream infile; //Opens a file
 
 public:
-    void storeWords()
+    void storeWords() //Reads words from txt file and stores them in a vector
     {
         infile.open("listofwords.txt");
 
-        if (infile.fail())
+        if (infile.fail()) //Error occurs and exits program if file isn't found
         {
             cerr << "Error: File Not Found" << endl;
             exit(1);
         }
 
-        while (!infile.eof())
+        while (!infile.eof()) //Pushes words in vector
         {
             infile >> word;
             list.push_back(word);
         }
     }
 
-    void printWords()
+    void printWords() //Prints words to check if they are stored in vector
     {
         for (int i = 0; i < list.size(); i++)
         {
-            //return list.at(i);
-
             cout << list.at(i) << endl;
         }
     }
--- WheelOfFortune/Main.cpp
+++ WheelOfFortune/Main.cpp
@@ -1,3 +1,10 @@
+/*
+Name: Austin Krause
+Class: CSCE 306
+Description: Wheel Of Fortune - A game where 
+two users guess phrases and earn money to win
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
